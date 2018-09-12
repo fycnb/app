def18a555c15bcf651e6ac3789bfd5618727bc31
yimoxiaohan <1044254212@qq.com>
2018-09-12 21:05:08 +0800

    1231

--- School/gen/com/example/school/R.java
+++ School/gen/com/example/school/R.java
@@ -48,11 +48,13 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int activity_main1=0x7f030002;
         public static final int activity_other=0x7f030003;
-        public static final int tab_icon=0x7f030004;
+        public static final int activity_welcome=0x7f030004;
+        public static final int tab_icon=0x7f030005;
     }
     public static final class menu {
         public static final int first=0x7f080000;
         public static final int main=0x7f080001;
+        public static final int welcome=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -61,6 +63,7 @@ public final class R {
         public static final int title_activity_first=0x7f050003;
         public static final int title_activity_main1=0x7f050005;
         public static final int title_activity_other=0x7f050004;
+        public static final int title_activity_welcome=0x7f050006;
     }
     public static final class style {
         /** 
