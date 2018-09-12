d5e83b266107ac42cfd277829f1f345b0e7016f3
fycnb <1162054822@qq.com>
2018-09-12 21:09:45 +0800

    123

--- School/src/com/example/school/Main123123Activity.java
+++ School/src/com/example/school/Main123123Activity.java
@@ -5,11 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Main123123Activity extends Activity {
+public class Main123123Activity  {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main123123);
-	}
 }
