c4365166d6c7171e68953cf3ea071581fa996214
fycnb <1162054822@qq.com>
2018-09-12 20:51:24 +0800

    12

--- School/gen/com/example/school/R.java
+++ School/gen/com/example/school/R.java
@@ -46,7 +46,9 @@ public final class R {
     public static final class layout {
         public static final int activity_first=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int tab_icon=0x7f030002;
+        public static final int activity_main1=0x7f030002;
+        public static final int activity_other=0x7f030003;
+        public static final int tab_icon=0x7f030004;
     }
     public static final class menu {
         public static final int first=0x7f080000;
@@ -57,6 +59,8 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int title_activity_first=0x7f050003;
+        public static final int title_activity_main1=0x7f050005;
+        public static final int title_activity_other=0x7f050004;
     }
     public static final class style {
         /** 
